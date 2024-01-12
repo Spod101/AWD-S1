3c577a9e863e23e4be9abf31cae1fbab022c8e9e
Eri Zanaiah <153974487+naiahhh@users.noreply.github.com>
2024-01-12 17:48:52 +0800

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,17 @@
+function changeColor() {
+    var container = document.querySelector('.container');
+    container.style.backgroundColor = '#FDFFAB';
+
+    var upper = document.querySelector('.upper-section');
+    upper.style.backgroundColor = '#4F6F52';
+    upper.style.color = '#D2E3C8';
+
+    var line = document.querySelector('.line');
+    line.style.backgroundColor = '#FDFFAB';
+
+
+
+    
+
+ 
+}
